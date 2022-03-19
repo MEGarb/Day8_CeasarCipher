10a9557bf6a9dd6361420676341ddbbed4bb19d2
MEGarb <mik.garber@gmail.com>
2022-03-19 00:38:30 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import art
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', ' ', '0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', ' ', '0','1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+def caesar(in_text, shift_amount, direction):
+    out_text = ""
+    for letter in in_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            if direction == "encode":
+                new_position = position + shift_amount
+            elif direction == "decode":
+                new_position = position - shift_amount
+            else:
+                print("Invalid input for direction.")
+                return
+            new_letter = alphabet[new_position]
+            out_text += new_letter
+        else:
+            out_text +=  letter
+
+    print(f"The {direction}d text is:  {out_text}")
+
+
+print(art.logo)
+again = "y"
+while again == "y":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = int(shift % (len(alphabet) / 2))
+    caesar(text, shift, direction)
+    again = input("Type 'y' to continue.")
